f5a1f52fc4a397e278712d3c5912d60cf316b181
David Carpenfelt <Carpen97@gmail.com>
2025-04-28 19:19:51 +0200

    Added Wrench-tool to mimich right-click. Added copy-tool and delete-tool

--- Assets/Scripts/Graphics/UI/MobileUIController.cs
+++ Assets/Scripts/Graphics/UI/MobileUIController.cs
@@ -1,4 +1,5 @@
 using DLS.Game;
+using DLS.Graphics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,11 +7,22 @@ using UnityEngine.UI;
 public class MobileUIController : MonoBehaviour
 {
 	[Header("Placement Buttons")]
-	public GameObject confirmButton; // Assign in Inspector
-	public GameObject cancelButton;  // Assign in Inspector
+	public GameObject confirmButton; 
+	public GameObject cancelButton; 
+	public GameObject wrenchTool;  
+	public GameObject trashCanTool;  
+	public GameObject copyTool;  
+	public bool isWrenchToolActive;
+	public GameObject boxSelectTool;  
+	public bool isBoxSelectToolActive;
+
+	private Image wrenchImage;
+	private Image boxSelectImage;
 
 	private System.Action onConfirmCallback;
 	private System.Action onCancelCallback;
+
+	public Text text;
 
 	public static MobileUIController Instance { get; private set; }
 
@@ -19,6 +31,8 @@ public class MobileUIController : MonoBehaviour
 		if (Instance == null)
 		{
 			Instance = this;
+			wrenchImage = wrenchTool.GetComponent<Image>();
+			boxSelectImage = boxSelectTool.GetComponent<Image>();
 		}
 		else
 		{
@@ -28,8 +42,27 @@ public class MobileUIController : MonoBehaviour
 		HidePlacementButtons(); // Already hiding buttons at start
 	}
 
-	// Call this when starting placement
-	public void ShowPlacementButtons(System.Action onConfirm, System.Action onCancel)
+    void Update()
+    {
+		bool inMenu = !(UIDrawer.ActiveMenu is UIDrawer.MenuType.None or UIDrawer.MenuType.BottomBarMenuPopup or UIDrawer.MenuType.ChipCustomization);
+		if(inMenu){
+			confirmButton.SetActive(false);
+			cancelButton.SetActive(false);
+			wrenchTool.SetActive(false);
+			boxSelectTool.SetActive(false);
+			trashCanTool.SetActive(false);
+			copyTool.SetActive(false);
+		}else{
+			wrenchTool.SetActive(true);
+			boxSelectTool.SetActive(true);
+			bool temp = Project.ActiveProject.controller.SelectedElements.Count>0 && !Project.ActiveProject.controller.IsPlacingElements;
+			trashCanTool.SetActive(temp);
+			copyTool.SetActive(temp);
+		}
+    }
+
+    // Call this when starting placement
+    public void ShowPlacementButtons(System.Action onConfirm, System.Action onCancel)
 	{
 		onConfirmCallback = onConfirm;
 		onCancelCallback = onCancel;
@@ -47,7 +80,41 @@ public class MobileUIController : MonoBehaviour
 		onCancelCallback = null;
 	}
 
-	// Hook these to the button OnClick() events
+	public void OnWrenchButtonPress()
+	{	
+		isWrenchToolActive = !isWrenchToolActive;
+		if(isWrenchToolActive){
+			isBoxSelectToolActive = false;
+			boxSelectImage.color = Color.white;
+			wrenchImage.color = Color.yellow;
+		}
+		else
+			wrenchImage.color = Color.white;
+	}
+
+	public void OnBoxSelectToolPress()
+	{	
+		isBoxSelectToolActive = !isBoxSelectToolActive;
+		if(isBoxSelectToolActive){
+			isWrenchToolActive = false;
+			wrenchImage.color = Color.white;
+			boxSelectImage.color = Color.yellow;
+		}
+		else
+			boxSelectImage.color = Color.white;
+	}
+	public void OnTrashCanPress()
+	{
+		Project.ActiveProject.controller.DeleteSelected();
+		HidePlacementButtons();
+	}
+
+	public void OnCopyToolPress()
+	{
+		Project.ActiveProject.controller.DuplicateSelectedElements();
+		Project.ActiveProject.controller.MoveSelectionAfterDuplication();
+	}
+
 	public void OnConfirmButtonPressed()
 	{
 		onConfirmCallback?.Invoke();
